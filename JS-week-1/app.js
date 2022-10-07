2017a3a1492be4695908b926aa6e12234c959b50
ediiotero <e.otero81@gmail.com>
2022-10-06 20:21:57 -0400

    js week 1 done

---
+++ JS-week-1/app.js
@@ -0,0 +1,20 @@
+// var name = "Eddie"
+// var number = 1
+// var batman = true
+
+// var test = ['Tobi', 3, false]
+
+var num1 = "5";
+var num2 = 5;
+var num3 = 2;
+
+console.log(num2 + num3);
+console.log(num2 - num3);
+console.log(num2 / num3);
+console.log(num2 * num3);
+console.log(num2 % num3);
+console.log(num2 < num3);
+console.log(num2 > num3);
+
+console.log("test 1", num1 == num2);
+console.log("test 2", num1 === num2);
