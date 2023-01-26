2f540a5c144aaa53f6c705de8a83d81d1d019d80
ediiotero <e.otero81@gmail.com>
2023-01-26 17:01:56 -0500

    week 14 ready

---
+++ React-week-14/src/routes/StateSample.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import StateColor from './StateColor'
+import PropSample from './PropSample'
+import StateCounter from './StateCounter'
+import ClickWord from './ClickWord'
+
+const StateSample = () => {
+    return (
+        <div>
+           State Sample
+        </div>
+    )
+}
+
+export default StateSample
